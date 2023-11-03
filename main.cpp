f41460e71b93d1d636e3fef1fbb342bce10c5df6
seifzeidan <szizoberlin@gmail.com>
2023-11-03 17:05:55 +0200

    Update main.cpp
    Adding the getAverage()

--- main.cpp
+++ main.cpp
@@ -9,12 +9,24 @@ int getSum(const std::vector<int>& vec) {
     }
     return sum;
 }
+double getAverage(const std::vector<int>& vec) {
+    if (vec.empty()) {
+        return 0.0; 
+    }
+
+    int sum = getSum(vec); 
+    double average = static_cast<double>(sum) / vec.size();
+    return average;
+}
+
 
 int main() {
     
     std::vector<int> elements = {2, 0, 1, 3, 4, 5, 22, 43, 12, 56};
     int sum = getSum(elements);
     std::cout << "Sum of elements : " << sum << std::endl;
+    double average = getAverage(elements);
+    std::cout << "Average of elements : " << average << std::endl;	
 
     return 0;
 }
